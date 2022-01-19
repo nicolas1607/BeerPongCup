7ed4753067aa60fc61e4308e6a73f14fd29f3012
Nicolas Mormiche <nicolas160796@gmail.com>
2022-01-19 16:42:36 +0100

    [CONFIG] Ajout de router et vuetify

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    outputDir: '/home/user/cordovatest/www',
+    publicPath: './',
+
+    pluginOptions: {
+      vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+    }
+}
